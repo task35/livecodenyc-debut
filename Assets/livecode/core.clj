81d28efde7fd1da47a4012e0f62ffff371b183cd
timsgardner <timsgardner@gmail.com>
2016-07-14 23:22:22 -0400

    gutting livecode.core

--- Assets/livecode/core.clj
+++ Assets/livecode/core.clj
@@ -4,13 +4,3 @@
   (:import [UnityEngine Rigidbody Vector3 Vector2 GameObject Component Transform]
            [clojure.lang Var]))
 
-(defn demo []
-  (position (v2 3))
-  (position (v3 0 3 1))
-  (position (object-named "Main Camera"))
-  (map position (seq (.transform (object-named "Canvas"))))
-  (def camera (object-named "Main Camera"))
-  (position #'camera))
-
-(defn force! [obj f]
-  (.AddForce (ensure-component obj Rigidbody) f))
