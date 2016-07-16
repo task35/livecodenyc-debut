c400db18805d4020771553709487e22f04368e44
Ramsey Nasser <ram@nas.sr>
2016-07-16 11:54:16 -0400

    Add Helpers

---
+++ Assets/Helpers.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Helpers {
+	public static GameObject Raycast (Vector3 origin, Vector3 direction, int layer)
+	{
+		RaycastHit hit;
+		if (Physics.Raycast (origin, direction, out hit, (1 << layer))) {
+			return hit.collider.gameObject;
+		} else {
+			return null;
+		}
+	}
+}
