5c98914bf01875a8c31fe3154513f26d1669fa59
Ramsey Nasser <ram@nas.sr>
2016-07-16 12:19:59 -0400

    Add tile-at

--- Assets/Helpers.cs
+++ Assets/Helpers.cs
@@ -1,14 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Helpers {
+public class Helpers
+{
 	public static GameObject Raycast (Vector3 origin, Vector3 direction, int layer)
 	{
+		Debug.Log ("Raycast");
 		RaycastHit hit;
-		if (Physics.Raycast (origin, direction, out hit, (1 << layer))) {
+		if (Physics.Raycast (origin, direction, out hit, Mathf.Infinity, (1 << layer), QueryTriggerInteraction.UseGlobal)) {
+			Debug.Log ("hit");
+			Debug.Log ("hit collider" + hit.collider);
 			return hit.collider.gameObject;
 		} else {
 			return null;
 		}
+	}
+
+	public static GameObject RaycastAll (Ray ray, int layer)
+	{
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit, (1 << layer))) {
+			return hit.collider.gameObject;
+		}
+
+		return null;
 	}
 }
